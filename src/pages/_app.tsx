8f62916f540663e63176c75842c68c54aaf542df
unknown <Sistemas@TICRAMIROPC.DataCenterLESP.local>
2023-03-27 15:17:40 -0600

    new pages and new Head... 1hr 58min youtube

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,18 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Toaster position="bottom-center" />
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
-};
+const MyApp: AppType = ({ Component, pageProps }) => (
+  <ClerkProvider {...pageProps}>
+    <Head>
+      <title>Chirp</title>
+      <meta name="description" content="Eh" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Toaster position="bottom-center" />
+    <Component {...pageProps} />
+  </ClerkProvider>
+);
 
 export default api.withTRPC(MyApp);
