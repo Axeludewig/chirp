1e08c81dd104674c1a9672d5036e5d1a0f99aa6d
unknown <Sistemas@TICRAMIROPC.DataCenterLESP.local>
2023-04-03 11:06:42 -0600

    separate ssg helper, full post page uwu

---
+++ src/server/helpers/ssgHelper.ts
@@ -0,0 +1,14 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postview";
+
+export const generateSSGHelper = () =>
+  createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
